8a7494b0836322ae64f00771e239937637bc61f1
Howard Pifer <paradx34@outlook.com>
2023-11-28 20:45:06 -0500

    created first function

--- index.js
+++ index.js
@@ -11,5 +11,11 @@ const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
 const dbName = process.env.DB_NAME;
 
-
-userPrompt();
+async function dbConnection(select) {
+    try {
+      const db = await mysql.createConnection({
+        host: "localhost",
+        user: dbUser,
+        password: dbPassword,
+        database: dbName,
+      });
