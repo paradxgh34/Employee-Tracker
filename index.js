f49b1a67b4c2c340a462a93b79830a1e9136169c
Howard Pifer <paradx34@outlook.com>
2023-11-28 20:43:00 -0500

    updated Dotenv variables

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const mysql = require("mysql2/promise");
 // Encryption for env file
 require("dotenv").config();
 
+
 // Dotenv variables
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
 const dbName = process.env.DB_NAME;
-
 
 
 userPrompt();
